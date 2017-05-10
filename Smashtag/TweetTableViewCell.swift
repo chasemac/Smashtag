09a0dd36d26342557f08daf4ae4267f50d3c0006
Chase McElroy <ichasem@gmail.com>
2017-05-09 21:53:12 -0700

    created twitter client that searches for hashtags

---
+++ Smashtag/TweetTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  TweetTableViewCell.swift
+//  Smashtag
+//
+//  Created by Chase McElroy on 5/9/17.
+//  Copyright © 2017 Chase McElroy. All rights reserved.
+//
+
+import UIKit
+
+class TweetTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}

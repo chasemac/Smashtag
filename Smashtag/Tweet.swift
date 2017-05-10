ad97568d0be97721862472d18df3e63fb62ed96a
Chase McElroy <ichasem@gmail.com>
2017-05-10 11:58:35 -0700

    setup coredata data model, classes and extensions

---
+++ Smashtag/Tweet.swift
@@ -0,0 +1,13 @@
+//
+//  Tweet.swift
+//  Smashtag
+//
+//  Created by Chase McElroy on 5/9/17.
+//  Copyright © 2017 Chase McElroy. All rights reserved.
+//
+
+import UIKit
+
+class Tweet: NSManagedObject {
+
+}
---
+++ Smashtag/TwitterUser.swift
@@ -0,0 +1,13 @@
+//
+//  TwitterUser.swift
+//  Smashtag
+//
+//  Created by Chase McElroy on 5/9/17.
+//  Copyright © 2017 Chase McElroy. All rights reserved.
+//
+
+import UIKit
+
+class TwitterUser: NSManagedObject {
+
+}
